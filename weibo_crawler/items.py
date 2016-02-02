4831865b53f3a1a60fc05c5ada57bb7f8e8e3348
kabessC <chenjunkai1024@outlook.com>
2016-02-03 05:21:09 +0800

    Add items object

--- weibo_crawler/items.py
+++ weibo_crawler/items.py
@@ -11,4 +11,16 @@ from scrapy.item import Item, Field
 class WeiboCrawlerItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    uid = Field()
+    u_id = Field()
+    u_name = Field()
+    u_weibo_count = Field()
+    u_following = Field()
+    u_fans = Field()
+    u_sex = Field()
+    u_region = Field()
+    u_birthday = Field()
+    u_introduction = Field()
+    u_tags = Field()
+    weibo = Field()
+    learn_experience = Field()
+    work_experience = Field()
